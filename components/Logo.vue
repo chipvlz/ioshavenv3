52726488af5a37dc19819a7621ce5aa7a74a62b0
Unknown <zackbaldwin148@gmail.com>
2017-11-06 18:13:30 -0500

    Added particlesJS to the homepage

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,79 +1,46 @@
-<template>
-  <div class="VueToNuxtLogo">
-    <div class="Triangle Triangle--two"></div>
-    <div class="Triangle Triangle--one"></div>
-    <div class="Triangle Triangle--three"></div>
-    <div class="Triangle Triangle--four"></div>
+<template lang="html">
+  <div>
+    <div id="particles-js"></div>
+    <div id="logo" class="p7 mt9"></div>
   </div>
 </template>
 
-<style>
-.VueToNuxtLogo {
-  display: inline-block;
-  animation: turn 2s linear forwards 1s;
-  transform: rotateX(180deg);
-  position: relative;
-  overflow: hidden;
-  height: 180px;
-  width: 245px;
-}
-
-.Triangle {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 0;
-  height: 0;
-}
-
-.Triangle--one {
-  border-left: 105px solid transparent;
-  border-right: 105px solid transparent;
-  border-bottom: 180px solid #41B883;
-}
-
-.Triangle--two {
-  top: 30px;
-  left: 35px;
-  animation: goright 0.5s linear forwards 3.5s;
-  border-left: 87.5px solid transparent;
-  border-right: 87.5px solid transparent;
-  border-bottom: 150px solid #3B8070;
-}
-
-.Triangle--three {
-  top: 60px;
-  left: 35px;
-  animation: goright 0.5s linear forwards 3.5s;
-  border-left: 70px solid transparent;
-  border-right: 70px solid transparent;
-  border-bottom: 120px solid #35495E;
-}
-
-.Triangle--four {
-  top: 120px;
-  left: 70px;
-  animation: godown 0.5s linear forwards 3s;
-  border-left: 35px solid transparent;
-  border-right: 35px solid transparent;
-  border-bottom: 60px solid #fff;
-}
-
-@keyframes turn {
-  100% {
-    transform: rotateX(0deg);
+<script>
+export default {
+  head () {
+    return {
+      script: [{src: '/particles.min.js'}]
+    }
+  },
+  mounted () {
+    window.onload = function () {
+      window.particlesJS.load('particles-js', '/particles-js.json', function () {
+        let particlesLoad = document.getElementById('particles-js')
+        particlesLoad.style.position = 'absolute'
+        particlesLoad.style.top = '0'
+        particlesLoad.style.left = '0'
+        particlesLoad.style.width = '100%'
+        particlesLoad.style.height = '100%'
+      })
+    }
   }
 }
+</script>
+<!-- <script src="/particles.min.js"></script>
 
-@keyframes godown {
-  100% {
-    top: 180px;
-  }
-}
+<script> particlesJS.load('particles-js', '/particles-js.json', function() {
+  console.log('callback - particles.js config loaded');
+});></script> -->
+<style lang="css" scoped>
+ #particles-js {
+   background-color: #21b7d7;
+   z-index: -1;
+ }
 
-@keyframes goright {
-  100% {
-    left: 70px;
-  }
-}
+ #logo {
+   background-image: url('/logo.svg');
+   background-repeat: no-repeat;
+   background-position: center;
+
+ }
 </style>
